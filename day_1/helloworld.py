2c622504cb65b2e25647b5d4a5a1ce56fb36cbfd
root <root@DESKTOP-7CO9QQK>
2023-07-05 19:19:36 +0200

    day_1 done

---
+++ day_1/helloworld.py
@@ -0,0 +1,25 @@
+import math
+
+print(2 + 3)
+print(2 - 3)
+print(2 * 3)
+print(2 % 3)
+print(2 / 3)
+print(2 ** 3)
+print(2 // 3)
+
+print("Lucien")
+print("Gabet")
+
+print(type(10))
+print(type(9.8))
+print(type(3.14))
+print(type(4 - 4j))
+print(type(['Asabeneh', 'Python', 'Finland']))
+print(type("Lucien"))
+print(type(True))
+print(type({'nom' : 'Lucien'}))
+print(type({1.2, 3, 4}))
+print(type((1.2, 3, 4)))
+
+print(math.sqrt(((10 - 2) ** 2) + ((8 - 3) ** 2)))
