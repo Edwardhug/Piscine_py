e2f2f95ff3d8e1b9e2d33939b87743b52c1d9b2b
root <root@DESKTOP-7CO9QQK>
2023-07-06 16:11:11 +0200

    day_2 kind of finish

---
+++ day_2/variables.py
@@ -0,0 +1,6 @@
+# Day 2: 30 Days of python programming
+
+first_name = raw_input("Enter first name : ")
+last_name = raw_input("Enter last name : ")
+full_name = first_name + ' ' + last_name
+print(full_name)
