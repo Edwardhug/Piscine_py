cc22382820b95986d975bd8235ad02f434f0ac62
root <root@DESKTOP-7CO9QQK>
2023-07-06 16:54:07 +0200

    day 3 done

---
+++ day_3/day_3.py
@@ -0,0 +1,7 @@
+# Day 3: 30 Days of python programming
+
+side_1 = int(raw_input("Enter first side :"))
+side_2 = int(raw_input("Enter second side :"))
+side_3 = int(raw_input("Enter third side :"))
+
+print("Perimeter = " + str(side_1 + side_2 + side_3))
